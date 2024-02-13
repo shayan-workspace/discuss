bef9733280bcec12b491a16f1b45d84ca357f9f3
shayan <shayan.workspace@gmail.com>
2024-02-13 22:56:44 +0500

    Github to Google Sign In

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,13 +1,13 @@
 import NextAuth, { Session, User } from "next-auth";
-import GitHub from "@auth/core/providers/github";
+import Google from "@auth/core/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/db";
 
-const AUTH_GITHUB_ID = process.env.AUTH_GITHUB_ID;
-const AUTH_GITHUB_SECRET = process.env.AUTH_GITHUB_SECRET;
+const AUTH_GOOGLE_ID = process.env.AUTH_GOOGLE_ID;
+const AUTH_GOOGLE_SECRET = process.env.AUTH_GOOGLE_SECRET;
 
-if (!AUTH_GITHUB_ID || !AUTH_GITHUB_SECRET) {
-  throw new Error("Missing Github OAuth Credentials!");
+if (!AUTH_GOOGLE_ID || !AUTH_GOOGLE_SECRET) {
+  throw new Error("Missing Google OAuth Credentials!");
 }
 
 export const {
@@ -18,9 +18,9 @@ export const {
 } = NextAuth({
   adapter: PrismaAdapter(db),
   providers: [
-    GitHub({
-      clientId: AUTH_GITHUB_ID,
-      clientSecret: AUTH_GITHUB_SECRET,
+    Google({
+      clientId: AUTH_GOOGLE_ID,
+      clientSecret: AUTH_GOOGLE_SECRET,
     }),
   ],
   callbacks: {
